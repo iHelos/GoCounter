02f529e37a15db587b312d5e867864870cc99773
ihelos <ihelos.ermakov@gmail.com>
2017-04-24 20:01:06 +0300

    forgot to add task

---
+++ task/task.go
@@ -0,0 +1,37 @@
+package task
+
+import (
+	"fmt"
+	"net/http"
+	"io/ioutil"
+	"regexp"
+)
+
+type Task struct {
+	Url    string
+	Result *chan int
+	RegRule     *regexp.Regexp
+}
+
+func (t *Task) Execute() {
+	response, err := http.Get(t.Url)
+	if err != nil {
+		t.ProcessError(err)
+		return
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		t.ProcessError(err)
+		return
+	}
+	count := len(t.RegRule.FindAll(body, -1))
+	//fmt.Println(string(body))
+	fmt.Printf("Count for %s: %d\n", t.Url, count)
+	*(t.Result) <- count
+}
+
+func (t *Task) ProcessError(err error) {
+	fmt.Printf("Error for %s: %s", t.Url, err.Error())
+	*(t.Result) <- 0
+}
